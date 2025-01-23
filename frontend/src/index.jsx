06a1ff58ccc24f6c77cf2c842d764f7c1283a1a4
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-01-23 10:41:45 -0300

    Configurando o Redux Dev Tools

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,10 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './main/reducers'
 
-const store = createStore(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+&& Window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
